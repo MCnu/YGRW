0999f0c9d2cfad0809c7871d0097a05e8f6d835a
Steven T <torrisi@g.harvard.edu>
2020-08-31 21:44:47 -0400

    Update angle correlation & steps function

--- steps.py
+++ steps.py
@@ -1,4 +1,10 @@
 import numpy as np
+from scipy.stats import gengamma
+import os as os
+from random import sample
+
+CUR_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(CUR_DIR, "data")
 
 
 class Stepper(object):
@@ -39,3 +45,59 @@ class GaussianSteps(Stepper):
 
     def generate_bound_step(self):
         return np.random.normal(loc=self.mu, scale=self.sig / 2, size=2)
+
+
+class GammaSteps(Stepper):
+    def __init__(
+        self,
+        shape: float = 0,
+        rate: float = 1,
+        bound_shape: float = None,
+        bound_rate: float = None,
+    ):
+        self.shape = shape
+        self.rate = rate
+
+        self.bound_shape = bound_shape or shape
+        self.bound_rate = bound_rate or rate
+
+        super().__init__()
+
+    def generate_step(self):
+        return gengamma.rvs(self.shape, self.rate, 1)
+
+    def generate_bound_step(self):
+        return gengamma.rvs(self.bound_shape, self.bound_rate, 1)
+
+
+
+class AngleStepper(object):
+    def __init__(self):
+        pass
+
+    def generate_angle(self):
+        raise NotImplementedError
+
+class UniformAngle(object):
+    def __init__(self):
+        pass
+
+    def generate_angle(self):
+
+        return np.random.uniform(low=-180, high=180, size=1)
+
+
+class ExperimentalAngle(object):
+    def __init__(self):
+
+        data_path = os.path.join(DATA_DIR, 'angle_correlation')
+        data = np.loadtxt(data_path, skiprows=1, delimiter=',')
+        self.x = data[:, 1]
+        self.y = data[:, 2]
+
+
+    def generate_angle(self):
+
+        angle = np.random.choice(self.x, size=1, p=self.y)
+        sign = sample([-1, 1], k=1)
+        return angle * sign
