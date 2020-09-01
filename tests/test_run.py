895e62f3a327111512e490e35bc29aecb3909339
Steven T <torrisi@g.harvard.edu>
2020-08-31 20:59:58 -0400

    New unit tests

---
+++ tests/test_run.py
@@ -0,0 +1,13 @@
+from run import generate_trajectory
+import numpy as np
+
+
+def test_basic_run():
+
+    traj = generate_trajectory(1)
+
+    assert len(traj) == 2
+    assert not np.array_equal(traj.position, traj.positions[0])
+
+    impossible_traj = generate_trajectory(1, nuclear_radius=0)
+    assert len(impossible_traj) == 1
